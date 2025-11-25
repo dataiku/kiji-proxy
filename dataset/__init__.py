009a6a82bed8583e3388ffe2c576aa38c249d591
Hannes Hapke <hanneshapke@users.noreply.github.com>
2025-11-24 19:59:38 -0800

    New model training to detect pii and coreferences (#15)
    * updated trainingset creation
    
    * update dataset generation
    
    * updated model training
    
    * linter fixes
    
    * linter updates
    
    * parallel dataset generation
    
    * moar linter fixes
    
    * updated  .github/workflows/lint.yml
    
    * reformatted
    
    * updated training, moved tokenization to the training
    
    * linter
    
    * updated quantization

---
+++ dataset/__init__.py
@@ -0,0 +1,5 @@
+"""Dataset generation module for PII training sets."""
+
+from .training_set import TrainingSetConfig, TrainingSetGenerator
+
+__all__ = ["TrainingSetConfig", "TrainingSetGenerator"]
