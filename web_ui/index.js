0f814225ce593445ae4c24e29a2ee201e1eefdb7
Hannes Hapke <hannes.hapke@gmail.com>
2025-11-24 20:06:08 -0800

    removed web_ui

--- web_ui/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PrivacyProxyUI from './privacy-proxy-ui.tsx';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<PrivacyProxyUI />);
