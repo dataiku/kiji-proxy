3660bb7a93acfb449aea0bf5ab5f15f9fe15ff2d
Hannes Hapke <hannes.hapke@gmail.com>
2026-03-30 15:02:09 -0700

    WIP

--- model/src/trainer.py
+++ model/src/trainer.py
@@ -158,7 +158,7 @@ class PIITrainer:
                 logging.warning(f"Warning: wandb not available ({e})")
                 self.config.use_wandb = False
 
-    def load_label_mappings(self, mappings: dict):
+    def load_label_mappings(self, mappings: dict, coref_info: dict | None = None):
         """Load label mappings from dataset processor."""
         self.pii_label2id = mappings["pii"]["label2id"]
         self.pii_id2label = {int(k): v for k, v in mappings["pii"]["id2label"].items()}
